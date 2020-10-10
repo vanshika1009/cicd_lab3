bef1a08b471684b66f713265251f3cbbb3316ae0
Vanshika <vanshikaaa1009@gmail.com>
2020-10-11 01:23:35 +0530

    Add files via upload

---
+++ src/main/java/Tools/MavenProject/App.java
@@ -0,0 +1,13 @@
+package Tools.MavenProject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/main/java/Tools/MavenProject/sample.java
@@ -0,0 +1,8 @@
+package Tools.MavenProject;
+
+public class sample {
+	public static void main(String args[])
+	{
+		System.out.println("Hello World!!!");
+	}
+}
